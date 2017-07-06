ce45e18d55aa9a56c00aa7e1aaa78dfd5fc57691
sunnydgoswami <sunnydgoswami@gmail.com>
2017-07-06 15:44:10 -0400

    chat context not appearing

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,16 +1,22 @@
 (function() {
-    function HomeCtrl($scope, Room, $uibModal) {
+    function HomeCtrl(Room, Message, $uibModal) {
         this.rooms = Room.all;
         console.log(this.rooms)
 
-        $scope.currentRoomId = ""
-
-        this.open = function () {
+        this.openNewRoom = function () {
           $uibModal.open({
               templateUrl: 'templates/modal.html',
               size: 'sm',
               controller: 'ModalCtrl as modal'
           });
+        };
+
+        this.currentRoom = null;
+
+        this.storeRoomName = function (room) {
+            this.currentRoom = room;
+            this.messages = Message.getByRoomId(this.currentRoom.$id)
+            console.log(this.messages)
         };
 
         // click function that sets current room based on the id of the room
@@ -19,5 +25,5 @@
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['$scope', 'Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
